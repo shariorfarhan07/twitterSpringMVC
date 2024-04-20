9217268f840be4c15949ccace46188d86cd190ba
shariorfarhan07 <md.sharior.hossain.farhan@g.bracu.ac.bd>
2024-04-21 05:33:15 +0600

    update

--- src/main/java/com/twitter/controller/FollowerController.java
+++ src/main/java/com/twitter/controller/FollowerController.java
@@ -1,17 +1,22 @@
 package com.twitter.controller;
 
 import com.twitter.config.DataSourceProperties;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+
 @Log
 @Controller
-//@RequestMapping("/add")
 public class FollowerController {
-    @GetMapping("/add")
-    public String followerMapping(@ModelAttribute("follow") String follow, @ModelAttribute("unfollow") String unfollow){
+//    @GetMapping("/add")
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String followerMapping(HttpSession session, @ModelAttribute("follow") String follow, @ModelAttribute("unfollow") String unfollow){
+//        HttpSession session = new Se
+        String userName =(String) session.getAttribute("username");
         log.info("follow : "+ follow+" unfollow : "+ unfollow);
+//        System.out.println("User : "+userName);
         DataSourceProperties db = new DataSourceProperties();
         log.info(String.valueOf(db));
 
